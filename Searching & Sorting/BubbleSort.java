28413190d048c7464e74823171ff75af7a9dbdb7
singhnitin01 <ns010696@gmail.com>
2021-01-25 21:32:29 +0530

    improved Bubble sort

--- Searching & Sorting/BubbleSort.java
+++ Searching & Sorting/BubbleSort.java
@@ -29,17 +29,25 @@ public class BubbleSort {
     public static void bubbleSort(int arr[]){
         int n = arr.length;
         for(int i=0; i < n-1; i++){
-            bubble(arr, i, n);
+            boolean swapped  = bubble(arr, i, n);
+            for(int j= 0; j< n; j++)
+                System.out.print(arr[j]+" ");
+            System.out.println();
+            if(!swapped)
+                break;
         }
     }
 
-    public static void bubble(int arr[], int i, int n){
+    public static boolean bubble(int arr[], int i, int n){
+        boolean swapped = false;
         for(int j = 1; j < n-i; j++){
             if(arr[j-1] > arr[j]){
                 int temp = arr[j-1];
                 arr[j-1] = arr[j];
                 arr[j] = temp;
+                swapped = true;
             }
         }
+        return swapped;
     }
 }
